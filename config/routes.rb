89b8336b5fd957d90686598680250505230db500
Arthur Perissich <arthurp@alumni.ie.edu>
2021-11-24 16:50:39 +0100

    ShotgunWithoutaccount + Modif en DB to make it work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/index'
-  devise_for :users
+  devise_for :users, controllers: {registrations: "registrations"}
   root to: 'pages#home'
   resources :lists, only: %I[index show new create destroy] do
     resources :shotguns, only: %I[create destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_163908) do
   end
 
   create_table "shotguns", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
