f3c47c06bcf1a8e556864b89398c7c49e434da5c
Alexis Jobart <jobart.pro@gmail.com>
2021-11-24 11:17:54 +0100

    fix the shotgunz

--- app/helpers/check_show_link_to_helper.rb
+++ app/helpers/check_show_link_to_helper.rb
@@ -4,6 +4,9 @@ module CheckShowLinkToHelper
       link_to "delete", list_path(@list), method: :delete
     else
       if @list.belongs_to_user?(current_user)
+        # Bouton delete my shotgunz à implanter
+        "ok"
+      else
         link_to "shotgunz !", list_shotguns_path(@list.id), method: :post
       end
     end
