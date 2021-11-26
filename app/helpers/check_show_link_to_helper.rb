01522e06f62284452f71acdd00a20a99ec1ff0e4
Arthur Perissich <arthurp@alumni.ie.edu>
2021-11-26 11:26:42 +0100

    Running correctly

--- app/helpers/check_show_link_to_helper.rb
+++ app/helpers/check_show_link_to_helper.rb
@@ -1,9 +1,9 @@
 module CheckShowLinkToHelper
   def check_shotgun_btn
     if current_user && @list.belongs_to_user?(current_user)
-      link_to "Delete my shotgun", list_shotgun_path(@list.id), method: :delete
+      link_to "Delete my shotgun", list_shotgun_path(@list.id), method: :delete, class: "shotgun-cta"
     else
-      link_to "Shotgun !", list_shotguns_path(@list.id), method: :post
+      link_to "Shotgun !", list_shotguns_path(@list.id), method: :post, class: "shotgun-cta"
     end
   end
 
